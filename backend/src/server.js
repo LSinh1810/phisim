e69a44665cc14cd6929e186bd52c8d1ce476a593
LSinh1810 <leesin17531@gmail.com>
2025-10-24 21:31:41 +0700

    server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -100,7 +100,7 @@ app.use("/api/track", trackRoutes);
 // Serve frontend build
 const frontendPath = path.join(__dirname, "../frontend/dist");
 app.use(express.static(frontendPath));
-app.get("*", (req, res) => {
+app.get("/.*/", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
